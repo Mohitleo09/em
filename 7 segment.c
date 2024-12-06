f88dcaa38137f7fd8314d6993d78e2aebc5d4453
Mohitleo09 <114983369+Mohitleo09@users.noreply.github.com>
2024-12-06 23:57:50 +0530

    Update 7 segment.c

--- 7 segment.c
+++ 7 segment.c
@@ -10,9 +10,10 @@ void delay(unsigned int count)
 void main()
 {
     unsigned char digits[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
+    unsigned char i; // Declare 'i' outside the loop
     while(1)
     {
-        for(unsigned char i = 0; i < 10; i++)
+        for(i = 0; i < 10; i++) // Use the declared variable here
         {
             P1 = digits[i];
             delay(1000);
