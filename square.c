a7b2dd160d0933a7ca84528fd737d95afd7146d1
Mohitleo09 <114983369+Mohitleo09@users.noreply.github.com>
2024-12-07 00:16:48 +0530

    Update square.c

--- square.c
+++ square.c
@@ -1,19 +1,21 @@
-#include <reg51.h>
+#include <reg51.h> // Include the 8051 register definition file
 
-void delay(unsigned int count)
-{
-    unsigned int i, j;
-    for(i = 0; i < count; i++)
-        for(j = 0; j < 1275; j++);
-}
+void delay();
 
-void main()
-{
-    while(1)
-    {
-        P1 = 0xFF;  // Square wave high
-        delay(500);
-        P1 = 0x00;  // Square wave low
-        delay(500);
+void main() {
+    while (1) {
+        P2 = 0x00; // Set Port 2 to 0x00
+        delay();   // Call delay function
+
+        P2 = 0xFF; // Set Port 2 to 0xFF
+        delay();   // Call delay function
     }
 }
+
+void delay() {
+    unsigned char r4, r3;
+    for (r4 = 188; r4 > 0; r4--) {     // Outer loop for delay
+        for (r3 = 255; r3 > 0; r3--) { // Inner loop for delay
+        }
+}
+		}
