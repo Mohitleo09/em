81b2ba4f10cede24067f992f544369315561fe0f
Mohitleo09 <114983369+Mohitleo09@users.noreply.github.com>
2024-12-06 22:24:59 +0530

    Update led toogle.c

--- led toogle.c
+++ led toogle.c
@@ -1,15 +1,18 @@
 #include <reg51.h>
 
-void delay(unsigned int);
-void delay(unsigned int x)
-{
-    while(x>0){
-x--;
+void delay(unsigned int x);
+
+void delay(unsigned int x) {
+    while (x > 0) {
+        x--;  // Simple delay loop
     }
 }
-void main(){
-    while(1){
-p1^0 = 0x01;
-delay(20000);
+
+void main() {
+    while (1) {
+        P1_0 = 1;       // Set P1.0 high
+        delay(20000);   // Delay
+        P1_0 = 0;       // Set P1.0 low
+        delay(20000);   // Delay
     }
 }
